465367ecca4821569677beeeb86fd83c78c3317c
Павел Кравченко <pavellevap98@mail.ru>
2019-03-25 02:35:42 +0700

    feature(EntityDAL) add DataContext

---
+++ Information_Systems_Architecture_2/DAL/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class DataContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Project> Projects { get; set; }
+
+        public DataContext()
+            : base("DefaultConnection")
+        { }
+    }
+}
