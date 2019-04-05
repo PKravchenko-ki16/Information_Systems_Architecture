5270bb2f3aec6d6dc32c629f190f707d56461a3b
Павел Кравченко <pavellevap98@mail.ru>
2019-04-05 15:20:32 +0700

    feature(BL) add BLEmployee & BLProject

---
+++ Information_Systems_Architecture_2/BL/BLEmployee.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using EntityDAL;
+using Model;
+
+namespace BL
+{
+    public class BLEmployee
+    {
+        private UnitOfWork entity;
+        public BLEmployee()
+        {
+          entity = new UnitOfWork();
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+           return entity.Employees.GetAll();
+        }
+
+        public void CreateEmployee(Employee employee)
+        {
+            entity.Employees.Create(employee);
+        }
+
+        public Employee GetByIdEmployee(int id)
+        {
+           return entity.Employees.Get(id);
+        }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            entity.Employees.Delete(employee);
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            entity.Employees.Update(employee);
+        }
+    }
+}
---
+++ Information_Systems_Architecture_2/BL/BLProject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using EntityDAL;
+using Model;
+
+
+namespace BL
+{
+    public class BLProject
+    {
+        private UnitOfWork entity;
+        public BLProject()
+        {
+            entity = new UnitOfWork();
+        }
+
+        public IEnumerable<Project> GetAllProjects()
+        {
+            return entity.Projects.GetAll();
+        }
+
+        public void CreateProject(Project project)
+        {
+            entity.Projects.Create(project);
+        }
+
+        public Project GetByIdEmployee(int id)
+        {
+            return entity.Projects.Get(id);
+        }
+
+        public void DeleteEmployee(Project project)
+        {
+            entity.Projects.Delete(project);
+        }
+
+        public void UpdateEmployee(Project project)
+        {
+            entity.Projects.Update(project);
+        }
+    }
+}
